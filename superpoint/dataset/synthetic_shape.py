5291a309c41a1379f5ffc648a36d5be2a349af0f
luo3300612 <591486669@qq.com>
2019-02-07 21:45:38 +0800

    update

--- superpoint/dataset/synthetic_shape.py
+++ superpoint/dataset/synthetic_shape.py
@@ -7,52 +7,43 @@ import cv2
 import json
 from pathlib import Path
 
-class SyntheticShape:
-    def __init__(self):
-        pass
 
-    def _init_dir(self, config):
-        pass
+def gen_data(primitive, config):
+    basedir = os.path.join(config['basedir'], primitive)
+    logging.info('Generating tarfile for primitive {}.'.format(primitive))
+    synthetic_dataset.set_random_state(np.random.RandomState(
+        config['generation']['random_seed']))
+    for split, size in config['generation']['split_sizes'].items():
+        im_dir, pts_dir = [Path(basedir, i, split) for i in ['images', 'points']]
 
-    def gen_data(self, primitive, config):
-        basedir = os.path.join(config['basedir'], primitive)
-        logging.info('Generating tarfile for primitive {}.'.format(primitive))
-        synthetic_dataset.set_random_state(np.random.RandomState(
-            config['generation']['random_seed']))
-        for split, size in config['generation']['split_sizes'].items():
-            im_dir, pts_dir = [Path(basedir, i, split) for i in ['images', 'points']]
+        im_dir.mkdir(parents=True, exist_ok=True)
+        pts_dir.mkdir(parents=True, exist_ok=True)
 
-            print("im_dir:", im_dir)
-            print("pts_dir:", pts_dir)
-            print("confirm?(y/n)")
-            c = input()
-            if c != "y":
-                exit()
+        for i in tqdm(range(size), desc=split, leave=False):
+            # gen origin image and point
+            image = synthetic_dataset.generate_background(
+                config['generation']['image_size'],
+                **config['generation']['params']['generate_background'])
+            points = np.array(getattr(synthetic_dataset, primitive)(
+                image, **config['generation']['params'].get(primitive, {})))
+            points = np.flip(points, 1)  # reverse convention with opencv
 
-            im_dir.mkdir(parents=True, exist_ok=True)
-            pts_dir.mkdir(parents=True, exist_ok=True)
+            # preprocess image and point
+            b = config['preprocessing']['blur_size']
+            image = cv2.GaussianBlur(image, (b, b), 0)
+            points = (points * np.array(config['preprocessing']['resize'], np.float)
+                      / np.array(config['generation']['image_size'], np.float))
+            image = cv2.resize(image, tuple(config['preprocessing']['resize'][::-1]),
+                               interpolation=cv2.INTER_LINEAR)
 
-            for i in tqdm(range(size), desc=split, leave=False):
-                image = synthetic_dataset.generate_background(
-                    config['generation']['image_size'],
-                    **config['generation']['params']['generate_background'])
-                points = np.array(getattr(synthetic_dataset, primitive)(
-                    image, **config['generation']['params'].get(primitive, {})))
-                points = np.flip(points, 1)  # reverse convention with opencv
+            # save
 
-                b = config['preprocessing']['blur_size']
-                image = cv2.GaussianBlur(image, (b, b), 0)
-                points = (points * np.array(config['preprocessing']['resize'], np.float)
-                          / np.array(config['generation']['image_size'], np.float))
-                image = cv2.resize(image, tuple(config['preprocessing']['resize'][::-1]),
-                                   interpolation=cv2.INTER_LINEAR)
-
-                cv2.imwrite(str(Path(im_dir, '{}.png'.format(i))), image)
-                np.save(str(Path(pts_dir, '{}.npy'.format(i))), points)
+            cv2.imwrite(str(Path(im_dir, '{}.png'.format(i))), image)
+            np.save(str(Path(pts_dir, '{}.npy'.format(i))), points)
 
 
 if __name__ == '__main__':
     with open("magic-point_shapes.json", 'r') as f:
         config = json.load(f)
-        dataset = SyntheticShape()
-        dataset.gen_data("draw_checkerboard", config)
+        for primitive in config['drawing_primitives']:
+            gen_data(primitive, config)
